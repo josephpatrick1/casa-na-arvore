34cd0f752c810c0df6f85d53a5c6844ee6a62c67
Joseph Patrick <josephpatrickrat@gmail.com>
2020-07-02 14:05:50 -0300

    seed

---
+++ backend/src/database/seeds/02_users_livros.ts
@@ -0,0 +1,14 @@
+import Knex from "knex"
+
+export async function seed(knex: Knex) {
+    return knex("users_livros").insert([
+        {
+            user_id: 1,
+            livros_id: 1
+        },
+        {
+            user_id: 2,
+            livros_id: 2
+        }
+    ])
+}
