a8feda67276fbe783f2e5833c4dd62830a088859
Rafael Fávaro Azzi <rafaelfavazzi@gmail.com>
2020-07-05 18:32:52 -0300

    Back update

--- backend/src/database/seeds/07_livros_categorias.ts
+++ backend/src/database/seeds/07_livros_categorias.ts
@@ -7,12 +7,28 @@ export async function seed(knex: Knex) {
             categoria_id: 5
         },
         {
+            livro_id: 1, // Pequeno principe
+            categoria_id: 1
+        },
+        {
             livro_id: 2, // De mal a pior
-            categoria_id: 3
+            categoria_id: 4
+        },
+        {
+            livro_id: 2, // De mal a pior
+            categoria_id: 1
         },
         {
             livro_id: 3, // Palavras Em Azul Profundo
             categoria_id: 5
+        },
+        {
+            livro_id: 3, // Palavras Em Azul Profundo
+            categoria_id: 4
+        },
+        {
+            livro_id: 3, // Palavras Em Azul Profundo
+            categoria_id: 1
         },
         {
             livro_id: 4, // O Herói Perdido 
@@ -20,7 +36,7 @@ export async function seed(knex: Knex) {
         },
         {
             livro_id: 5, // O Oráculo Oculto
-            categoria_id: 7
+            categoria_id: 3
         },
         {
             
@@ -43,6 +59,14 @@ export async function seed(knex: Knex) {
         {
             livro_id: 6,
             categoria_id: 1
-        }
+        },
+        {
+            livro_id: 6,
+            categoria_id: 5
+        },
+        {
+            livro_id: 6, // À Beira da Eternidade
+            categoria_id: 4
+        },
     ])
 }
