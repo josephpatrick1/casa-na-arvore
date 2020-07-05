6e0b4f699582de8f87af3585cb9fd7131adcc18f
Rafael Fávaro Azzi <rafaelfavazzi@gmail.com>
2020-07-05 19:06:35 -0300

    Seed correction

--- backend/src/database/seeds/05_users_amigos.ts
+++ backend/src/database/seeds/05_users_amigos.ts
@@ -8,15 +8,19 @@ export async function seed(knex: Knex) {
         },
         {
             user_id1: 1,
+            user_id2: 2
+        },
+        {
+            user_id1: 1,
             user_id2: 3
         },
         {
-            user_id1: 2,
-            user_id2: 5
+            user_id1: 1,
+            user_id2: 4
         },
         {
-            user_id1: 4,
-            user_id2: 2
+            user_id1: 1,
+            user_id2: 5
         }
     ])
 }
